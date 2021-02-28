bd367b7bee57f1084e59035d2d9c81728eea8a86
IshanJawade <ishankjawade@gmail.com>
2021-02-28 16:58:15 +0530

    Fibonacci code added

---
+++ fibonacciSeries.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+class FibonacciSeries{
+    static void Main(String[] args){
+    
+        var fibonacci = new List<int> {1,1};
+        
+        for (int i = 1; i <= 18; i++){
+
+            var prev = fibonacci[fibonacci.Count - 1];
+            var prev2 = fibonacci[fibonacci.Count - 2];        
+
+            fibonacci.Add(prev + prev2);
+        }
+        foreach(var num in fibonacci)
+            Console.Write($"{num} ");
+        Console.WriteLine($"");
+        Console.WriteLine($"Length of fibonacci series is: {fibonacci.Count}");
+    }
+}
