126f093a667fd90ef024c332c04b96904e99e0c5
IshanJawade <ishankjawade@gmail.com>
2021-09-24 17:56:18 +0530

    Just a commit

---
+++ ClassObject.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+class car {
+    public string PrintStuff(){
+        string str = "Its PrintStuff fucntion from car class";
+        return str;
+    }
+}
+
+class ClassObject {
+    public static void Main(string[] args){
+        car obj = new car();
+        Console.WriteLine(obj.PrintStuff());
+    }
+}
+
