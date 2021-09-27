0d3db61ab66882224789010e70849835a2d53b52
Ishan Jawade <43887753+IshanJawade@users.noreply.github.com>
2021-09-27 16:09:28 +0530

    Search in array Updated

--- SearchInArrat.cs
+++ SearchInArrat.cs
@@ -1,20 +1,24 @@
 using System;
 
 class SearchTheElement {
-    static bool EleIsPresent (int n, int[] arr){
-        for (int i=0; i < arr.Length; i++){
-            if (n == arr[i])
-                return true;
-            else
-                return false;
-        }
-    }
-    public static bool Main(){
+    public static void Main(){
         int[] arr = {15, 30, 45, 60, 75, 90, 105};
-
+        int index = 0;
+        bool flag=false;
         Console.WriteLine("Which number you want to find?");
         int findNum = Convert.ToInt32(Console.ReadLine());
-
-        EleIsPresent(findNum, arr);
+        for (int i=0; i < arr.Length; i++){
+            if (findNum == arr[i]){ 
+                flag = true;
+                break;
+            }   
+            else{
+                index ++;
+            }    
+        }
+        if (flag)
+            Console.WriteLine("Element is present at index {0}.", index);
+        else
+            Console.WriteLine("Element is not present.");
     }
 }
